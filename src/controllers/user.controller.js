ffd38a0afdbc1f0cfe2365b37b3b1976365fcecd
JayGangwar07 <jaygangwar86@gmail.com>
2025-04-12 15:06:01 +0530

    Register Started

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,8 +3,21 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 const registerUser = asyncHandler( async(req,res) => {
   
   console.log("Register endpoint hit");
+
+  // Get Details From Frontend
+  // Validate Data
+  // Check If Already Exists
+  // Check For Files Avatar And CoverImg
+  // Upload To Cloudinary
+  // Create User Object In db
+  // Remove Password And Refresh Token From Response
+  // Check If User was Created
+  // return res
+
+  const {email} = req.body
+
   res.status(200).json({
-    message: "ok"
+    message: email
   })
   
 } )
