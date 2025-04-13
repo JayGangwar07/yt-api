a02001bcfb242432279f2c7dc794e86520174e3d
JayGangwar07 <jaygangwar86@gmail.com>
2025-04-13 08:38:13 +0530

    User Controller Done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js"
+import {ApiError} from "../utils/ApiError.js"
+import { User } from "../models/user.models.js"
+import {cloudinaryUpload} from "../utils/cloudinary.js"
 
 const registerUser = asyncHandler( async(req,res) => {
   
@@ -14,7 +17,53 @@ const registerUser = asyncHandler( async(req,res) => {
   // Check If User was Created
   // return res
 
-  const {email} = req.body
+  const {
+    fullname,
+    email,
+    password,
+    username,
+  } = req.body
+  
+  if (
+    [fullname,email,password,username].some((i)=>i?.trim === "")
+    ) {
+      throw new ApiError(400,"All Fields Required")
+    }
+    
+  const existed = User.findOne(
+    $or: [{username},{email}]
+    )
+    
+  if (existed) {
+    throw new ApiError(409,"User With Same Email/Username Exists")
+  }
+  
+  const avatarLocalPath = req.files?.avatar[0]?.path
+  const coverImgLocalPath = req.files?.coverImg[0]?.path
+  
+  if (!avatarLocalPath){
+    throw new ApiError(400,"Avatar Is Required")
+  }
+  
+  const avatarPath = await cloudinaryUpload(avatarLocalPath)
+  const coverImgPath = await cloudinaryUpload(coverImgLocalPath)
+  
+  if (!avatarPath){
+    throw new ApiError(400,"Avatar Is Required")
+  }
+  
+  const user = await User.create({
+    fullname,
+    avatarPath: avatarPath.url,
+    coverImgPath: coverImgPath?.url || "",
+    password,
+    email,
+    username: username.toLowerCase()
+  })
+  
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+    )
 
   res.status(200).json({
     message: email
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,26 @@
 import { Router } from "express"
 import { registerUser } from "../controllers/user.controller.js"
+import { upload } from "../middlewares/multer.middlewares.js"
 
 const router = Router()
 
 console.log("User Router Mounted")
 
-router.post("/login",registerUser)
-router.route("/register").get(registerUser)
+// Probably Error
+
+router.post(
+  "/login",
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1
+    },
+    {
+      name: "coverImg",
+      maxCount: 1
+    }
+    ]),
+  registerUser
+  )
 
 export default router
