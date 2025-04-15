315604e37dfb29f6b7095e3c228551d2f66168c1
JayGangwar07 <jaygangwar86@gmail.com>
2025-04-15 20:01:22 +0530

    Token Commit 2

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 import {ApiError} from "../utils/ApiError.js"
 import { User } from "../models/user.models.js"
 import {cloudinaryUpload} from "../utils/cloudinary.js"
+import jwt from "jsonwebtoken"
 import {ApiResponse} from "../utils/ApiResponse.js"
 
 const generateAccessAndRefreshTokens = async (userId) => {
@@ -240,48 +241,52 @@ const logoutUser = asyncHandler(async(req,res) => {
   
 })
 
-const refreshAccessToken = asyncHandler(async(req,res) => {
-  
-  // Use Cookie To Access Token
-  
-  const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
-  
-  if (!incomingRefreshToken){
-    throw new ApiError(400,"No Refresh Token Found")
-  }
-  
-  const decodedToken = jwt.verify(
-    incomingRefreshToken,
-    process.env.REFRESH_TOKEN_SECREY
-    )
+const refreshAccessToken = asyncHandler(async (req, res) => {
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+
+    if (!incomingRefreshToken) {
+      throw new ApiError(401, "unauthorized request")
+    }
+
+    try {
+      const decodedToken = jwt.verify(
+      incomingRefreshToken,
+            process.env.REFRESH_TOKEN_SECRET
+        )
     
-  const user = await User.findById(decodedToken?._id)
-  
-  if (!user){
-    throw new ApiError(400,"Inavalid Refresh Token")
-  }
-  
-  if (incomingRefreshToken !== user?.refreshToken){
-    throw new ApiError(400,"Inavlid Refresh Token")
-  }
-  const {accessToken,newRefreshToken} = await generateAccessAndRefreshTokens(user?._id)
-  
-  const options = {
-    httpOnly: true,
-    secure: true
-  }
-  
-  return res
-  .status(200)
-  .cookie("accessToken",accessToken,options)
-  .cookie("refreshToken",newRefreshToken,options)
-  .json(
-    new ApiResponse(200,{
-      accessToken,
-      newRefreshToken
-    },"Refreshed Tokens")
-    )
-  
+      const user = await User.findById(decodedToken?._id)
+    
+      if (!user) {
+          throw new ApiError(401, "Invalid refresh token")
+        }
+    
+      if (incomingRefreshToken !== user?.refreshToken) {
+          throw new ApiError(401, "Refresh token is expired or used")
+            
+        }
+    
+      const options = {
+            httpOnly: true,
+            secure: true
+        }
+    
+      const {accessToken, newRefreshToken} = await generateAccessAndRefereshTokens(user._id)
+    
+      return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", newRefreshToken, options)
+        .json(
+            new ApiResponse(
+                200, 
+                {accessToken, refreshToken: newRefreshToken},
+                "Access token refreshed"
+            )
+        )
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid refresh token")
+    }
+
 })
 
 export { 
